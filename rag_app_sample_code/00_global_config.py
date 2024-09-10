d28d96cfb07275342900224cc67ce12cdf8cdac8
rohit-kg <71235574+rohit-kg@users.noreply.github.com>
2024-09-10 15:34:56 -0700

    Update default UC catalog and VS endpoint in 00_global_config.py

--- rag_app_sample_code/00_global_config.py
+++ rag_app_sample_code/00_global_config.py
@@ -34,7 +34,7 @@ RAG_APP_NAME = 'my_agent_app'
 
 # UC Catalog & Schema where outputs tables/indexs are saved
 # If this catalog/schema does not exist, you need create catalog/schema permissions.
-UC_CATALOG = f'{user_name}_catalog'
+UC_CATALOG = f'genai_workshop'
 UC_SCHEMA = f'rag_{user_name}'
 
 ## UC Model name where the POC chain is logged
@@ -42,7 +42,7 @@ UC_MODEL_NAME = f"{UC_CATALOG}.{UC_SCHEMA}.{RAG_APP_NAME}"
 
 # Vector Search endpoint where index is loaded
 # If this does not exist, it will be created
-VECTOR_SEARCH_ENDPOINT = f'{user_name}_vector_search'
+VECTOR_SEARCH_ENDPOINT = f'genai_workshop_vs_endpoint'
 
 # Source location for documents
 # You need to create this location and add files
