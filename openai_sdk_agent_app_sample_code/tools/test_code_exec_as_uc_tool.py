fc7042dfda2a940d3bb4478e136fba45e3297a6c
Sid Murching <sid.murching@databricks.com>
2024-11-18 21:03:27 -0800

    Skip some tests in GHA
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- openai_sdk_agent_app_sample_code/tools/test_code_exec_as_uc_tool.py
+++ openai_sdk_agent_app_sample_code/tools/test_code_exec_as_uc_tool.py
@@ -1,9 +1,14 @@
 import pytest
 from cookbook.tools.uc_tool import UCTool
+import os
 
 CATALOG = "ep"
 SCHEMA = "cookbook_local_test"
 
+
+if os.getenv("GITHUB_ACTIONS") == "true":
+    pytest.skip("Skipping all tests in this module in CI, as "
+                "Databricks auth is not available there", allow_module_level=True)
 
 @pytest.fixture
 def python_exec():
