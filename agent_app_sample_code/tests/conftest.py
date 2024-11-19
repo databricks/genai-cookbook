60fa2c1b8935eee38393c8f6b6a0f0b80cb3faae
Sid Murching <sid.murching@databricks.com>
2024-11-18 20:42:23 -0800

    Fix tests
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- agent_app_sample_code/tests/conftest.py
+++ agent_app_sample_code/tests/conftest.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-# Add the root directory to sys.path, so that we can treat directories like
-# agent_app_sample_code as modules
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+# Add the parent directory to sys.path, so that we can treat directories like
+# `utils` as modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
--- openai_sdk_agent_app_sample_code/tests/conftest.py
+++ openai_sdk_agent_app_sample_code/tests/conftest.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-# Add the root directory to sys.path, so that we can treat directories like
-# openai_sdk_agent_app_sample_code as modules
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+# Add the parent directory to sys.path, so that we can treat directories like
+# `cookbook` as modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
